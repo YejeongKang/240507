077077aa64f4651e140a25075e22b449ef990e48
YejeongKang <dpwjd1130@gmail.com>
2024-05-07 16:27:46 +0900

    주석

--- 240507/prac0.cpp
+++ 240507/prac0.cpp
@@ -8,29 +8,33 @@ using namespace std;
 int main() {
 	while (1) {
 
+		// hello.txt에 있는 문자열을 저장할 벡터 선언
 		vector<string> v;
 
+		// hello.txt와 output.txt의 문자열을 받아올 변수 선언
+		string line1;
+		string line2;
+
+		// hello.txt 파일을 담을 변수 선언
 		ifstream read_file1("hello.txt");
 
-		string line;
-		string print_txt;
-
+		// hello.txt 파일이 잘 열렸다면, 엔터를 기준으로 읽어오기 => 벡터 v에 저장
 		if (read_file1.is_open() == true) {
 
 			for (int i = 0; i < 3; i++) {
 
-				getline(read_file1, line);
+				getline(read_file1, line1);
 
-				v.push_back(line);
+				v.push_back(line1);
 
 			}
-		}
-		else {
-				break;
-			}
+		}	
+		else break;
 
+		// 'hello.txt의 문자열을 거꾸로 입력할 파일'을 담을 변수 선언
 		ofstream write_file("output.txt");
 
+		// write_file이 새로 생성되었는지 확인하고, 생성되지 않았으면 오류 메세지 출력
 		if (write_file.fail() == true) {
 		
 			cout << "생성된 파일 없음." << endl;
@@ -38,24 +42,31 @@ int main() {
 			break;
 		}
 		else {
-
+			// 벡터 v에 저장한 문자열을 write_file에 작성
 			write_file << v[2] << endl << v[1] << endl << v[0] << endl;
 		}
 
+		// output.txt 파일을 담을 변수 선언
 		ifstream read_file2("output.txt");
+		
+		// output.txt 파일이 잘 열렸다면, 엔터를 기준으로 읽어서 출력
+		if (read_file1.is_open() == true) {
+			for (int i = 0; i < 3; i++) {
 
-		for (int i = 0; i < 3; i++) {
+				getline(read_file2, line2);
 
-			getline(read_file2, print_txt);
+				cout << line2 << endl;
 
-			cout << print_txt << endl ;
-
+			}
 		}
+		else break;
 
+		// 파일 닫기
 		read_file1.close();
 		read_file2.close();
 		write_file.close();
 
+		// while문 탈출 야호 !
 		return 0;
 	}
 }
