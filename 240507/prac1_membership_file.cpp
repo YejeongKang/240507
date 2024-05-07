9e94229970cbd81c1271d0a653c3a3c055908729
YejeongKang <dpwjd1130@gmail.com>
2024-05-07 19:31:31 +0900

    done

--- 240507/prac1_membership_file.cpp
+++ 240507/prac1_membership_file.cpp
@@ -8,66 +8,81 @@ using namespace std;
 #define MEMBER 3
 
 int main() {
-	string mem_name;
+	int cnt = 0;
+	string str;
 	string line;
-	int mem_pw;
+	string pw_check;
+	bool isNum;
 	vector<string> v_name;
-	vector<int> v_pw;
+	vector<string> v_pw;
 
-	while (1) {
-		v_name.reserve(MEMBER);
-		v_pw.reserve(MEMBER);
-
+	while(1){
 		cout << MEMBER << "명의 회원에 대한 이름 비밀번호를 순차적으로 입력하세요." << endl;
 
-		for (int i = 0; i < MEMBER; i++) {
+		while (cnt < MEMBER) {
+				v_name.reserve(MEMBER);
+				v_pw.reserve(MEMBER);
 
-			cout << i + 1 << "번째 회원 : ";
+				cout << cnt + 1 << "번째 회원 : ";
 
-			cin >> mem_name >> mem_pw;
+				getline(cin, str);
 
-			v_name.push_back(mem_name);
-			v_pw.push_back(mem_pw);
+				pw_check = str.substr(str.find(" ") + 1);
 
-		}
+				for (char ch : pw_check) {
+					if (isdigit(ch) == false) {
+						cout << "입력오류: 비밀번호는 숫자만 입력하세요." << endl;
+						isNum = false;
+						break;
+					} else isNum = true;
+				}
+				if (isNum == true) {
+					v_name.push_back(str.substr(0, str.find(" ")));
 
-		ofstream member_info("member.txt");
+					v_pw.push_back(str.substr(str.find(" ") + 1));
 
-		if (member_info.fail() == true) {
-
-			cout << "생성된 파일 없음." << endl;
-
-			break;
-		}
-		else {
-			// 벡터 v(name,pw)에 저장한 문자열을 member_info에 작성
-			for (int i = 0; i < MEMBER; i++) {
-
-				member_info << v_name[i] << " " << v_pw[i] << endl;
+					cnt++;
+				}
 
 			}
 
-		ifstream member_print("member.txt");
-		
-		// output.txt 파일이 잘 열렸다면, 엔터를 기준으로 읽어서 출력
-		if (member_print.is_open() == true) {
-			cout << "--------- 회원 명부 출력 ----------";
-			for (int i = 0; i < MEMBER; i++) {
+			ofstream member_info("member.txt");
 
-				getline(member_print, line);
+			if (member_info.fail() == true) {
 
-				cout << line << endl;
+				cout << "생성된 파일 없음." << endl;
+
+				break;
+			}
+			else {
+				// 벡터 v(name,pw)에 저장한 문자열을 member_info에 작성
+				for (int i = 0; i < MEMBER; i++) {
+
+					member_info << v_name[i] << " " << v_pw[i] << endl;
 
 				}
+
+			ifstream member_print("member.txt");
+		
+			// output.txt 파일이 잘 열렸다면, 엔터를 기준으로 읽어서 출력
+			if (member_print.is_open() == true) {
+				cout << "--------- 회원 명부 출력 ----------" << endl;
+				for (int i = 0; i < MEMBER; i++) {
+
+					getline(member_print, line);
+
+					cout << line << endl;
+
+					}
+				}
+				else break;
+
+				member_info.close();
+				member_print.close();
+
+				// while문 탈출 야호 !
+				return 0;
+
 			}
-			else break;
-
-			member_info.close();
-			member_print.close();
-
-			// while문 탈출 야호 !
-			return 0;
-
 		}
 	}
-}
