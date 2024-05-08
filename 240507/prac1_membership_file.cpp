f5d72705485f35c46246294e9f02a91c752dcba5
YejeongKang <dpwjd1130@gmail.com>
2024-05-08 15:18:23 +0900

    prac1 주석, 반복문 수정

--- 240507/prac1_membership_file.cpp
+++ 240507/prac1_membership_file.cpp
@@ -14,34 +14,55 @@ using namespace std;
 
 int main() {
     int cnt = 0;
-    string str;
-    string pw;
-    vector<string> v_name;
-    vector<string> v_pw;
+    string str, name, pw;
+    bool input_check;
+    vector<string> v_name, v_pw;
+    size_t point, re_point;
 
     while (cnt < MEMBER) {
         cout << MEMBER - cnt << "명의 회원에 대한 이름, 비밀번호를 순차적으로 입력하세요.(비밀번호는 6-17자 이내로 작성)" << endl;
 
         cout << cnt + 1 << "번째 회원 : ";
+
         getline(cin, str);
 
-        // 입력 값 확인
-        size_t point = str.find(" ");
+        // 입력 값 (공백 하나 이상 두고 입력했는지) 확인
+        point = str.find(" ");
+
         if (point == string::npos) {
-            cout << "입력오류: 이름과 비밀번호를 입력하세요." << endl;
-            continue; // 다음 회원 정보 입력 받기
+
+            cout << "입력오류: 이름과 비밀번호를 공백을 두고 입력하세요." << endl;
+
+            input_check = false;
         }
 
-        // 이름과 비밀번호 추출
-        string name = str.substr(0, point);
-        pw = str.substr(point + 1);
+        // 입력 값 (공백 하나만 두고 입력했는지) 확인
+        else
+        {
+            re_point = str.find(" ", point);
 
-        // 비밀번호 길이 확인
-        if (pw.length() < 6 || pw.length() > 17) {
-            cout << "입력오류: 비밀번호는 6-17자 이내로 작성하세요." << endl;
-            continue; // 다음 회원 정보 입력 받기
+            if (re_point != string::npos) {
+
+                cout << "입력오류: 이름과 비밀번호 외의 정보는 입력할 수 없습니다." << endl;
+
+                input_check = false;
+            }
+            else {
+                // 이름과 비밀번호 추출
+                name = str.substr(0, point);
+                pw = str.substr(point + 1);
+
+                // 공백 하나를 두고 입력했다면 비밀번호 자릿수 확인
+                if (pw.length() < 6 || pw.length() > 17) {
+                    cout << "입력오류: 비밀번호는 6-17자 이내로 작성하세요." << endl;
+                 }
+                // 공백 하나, 비밀번호 자릿수 조건을 모두 충족했을 때, true
+                else { 
+                    input_check = true;
+                }
+            }
         }
-
+        
         // 벡터에 저장
         v_name.push_back(name);
         v_pw.push_back(pw);
@@ -51,30 +72,43 @@ int main() {
 
     // 파일에 회원 정보 저장
     ofstream member_info("member.txt");
+
     if (member_info.is_open()) {
+
         for (int i = 0; i < MEMBER; i++) {
+
             member_info << v_name[i] << " " << v_pw[i] << endl;
+
         }
         member_info.close();
     }
     else {
         cout << "파일을 열 수 없습니다." << endl;
-        return 1;
+
+        // while문 탈출
+        cnt = 10;
     }
 
     // 파일에서 회원 정보 출력
     ifstream member_print("member.txt");
+
     if (member_print.is_open()) {
+
         cout << "--------- 회원 명부 출력 ----------" << endl;
+
         while (getline(member_print, str)) {
+
             cout << str << endl;
         }
         member_print.close();
     }
     else {
+
         cout << "파일을 열 수 없습니다." << endl;
-        return 1;
+
+        // while문 탈출
+        cnt = 10;
     }
 
-    return 0;
+    cnt = 10;
 }
